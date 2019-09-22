734b4524b107cd6a88369c8de4a46828f9925925
Abdulghaphor Hajjieh <ghaphorhajjieh@gmail.com>
2019-09-22 21:20:04 +0300

    working on payment

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from cart.models import Cart
 # Create your models here.
+class Payment(models.Model):
+	cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='payment')
+	merchantcode = models.IntegerField(default=642616)
+	amount = models.PositiveIntegerField()
+	token = models.IntegerField(default=10000)
+	status = models.CharField(max_length=1,null=True,blank=True)
+	payment_token = models.CharField(max_length=20,null=True,blank=True)
+	payment_id = models.CharField(max_length=20,null=True,blank=True)
+	paid_on = models.CharField(max_length=20,null=True,blank=True)
+	method = models.CharField(max_length=1,null=True,blank=True)
+	administative_charge = models.CharField(max_length=20,null=True,blank=True)
+	
+	def __str__(self):
+		return  "%s: %s x %s = %s" % (self.cart.user, self.product.price, self.quantity, self.sub_total())
+
+
+# Status=1&
+# PaymentToken=641569175678&
+# PaymentId=100201926562131678&
+# PaidOn=2019-09-22+21%3A09%3A33&
+# Variable1=&
+# Variable2=&
+# Variable3=&
+# Variable4=&
+# Variable5=&
+# Method=1&
+# AdministrativeCharge=0
+
+# Status - Payment status (1 - success 0 - failure )
+# PaymentToken - Referance id
+# PaymentId - Payment id
+# PaidOn - Date time
+# Variable1 - Custom user parameter 1
+# Variable2 - Custom user parameter 2
+# Variable3 - Custom user parameter 3
+# Variable4 - Custom user parameter 4
+# Variable5 - Custom user parameter 5
+# Method=1 or 2 (KNET=1 and MIGs =2)
--- payment/views.py
+++ payment/views.py
@@ -4,21 +4,30 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 # Create your views here.
+from .models import Payment
 import requests
+
 class Generate(APIView):
 	def get(self, request, format=None):
 		cart = get_object_or_404(Cart, user=self.request.user, status='A')
-		baseurl = 'http://demo.hesabe.com/authpost'
-		payload = {
-		'MerchantCode': 642616, 
-		'Amount': 10,
-		'SuccessUrl': 'http://localhost:8001/payment/success/',
-		'FailureUrl': 'http://localhost:8001/payment/failure/',
-		}
+		payment = Payment.objects.create(cart=cart,amount=250)
+		if payment:
+			baseurl = 'http://demo.hesabe.com/authpost'
+			payload = {
+			'MerchantCode': payment.merchantcode, 
+			'Amount': payment.amount,
+			'SuccessUrl': 'http://localhost:8001/payment/success/%s' % payment.token,
+			'FailureUrl': 'http://localhost:8001/payment/failure/%s' % payment.token,
+			}
+			response = requests.post(baseurl, data=payload)
+			hesabe = response.json()
+			if hesabe['status'] == "success":
+				token = hesabe['data']['token']
+				payment_url = hesabe['data']['paymenturl'] + token
+				print(payment_url)
+				return Response(hesabe, status=status.HTTP_200_OK)
+		Response({'status':'payment failed'}, status=status.HTTP_400_BAD_REQUEST)
 
-		response = requests.post(baseurl, data=payload)
-		print(response.json())
-		return Response(response.json(), status=status.HTTP_200_OK)
 
 class Successful(APIView):
 	def get(self, request, format=None):
