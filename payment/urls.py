1c3ad8c3524f787674b4aa076fe96455b0cdb8bf
Abdulghaphor Hajjieh <ghaphorhajjieh@gmail.com>
2019-09-22 20:15:59 +0300

    fixed createnewcart

--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,7 @@ app_name = 'payment'
 urlpatterns = [
 	path('successful/',_.Successful.as_view(),name='successful'),
 	path('failed/',_.Failed.as_view(),name='failed'),
+	path('',_.Generate.as_view(),name='generate'),
 	# path('add/', _.Add.as_view(),name='add'),
 	# path('delete/', _.Delete.as_view(),name='delete'),
 	# path('edit/', _.Edit.as_view(),name='edit'),
